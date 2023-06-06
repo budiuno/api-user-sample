26b7a1d855dd08108f4305252c3f12903502ceb8
budiuno <dilapanga1@gmail.com>
2023-06-06 07:11:09 +0700

    remove SLACK URL

--- src/user/helpers/slack.ts
+++ src/user/helpers/slack.ts
@@ -5,7 +5,7 @@ const environment = process.env.ENVIRONMENT
 let  MY_SLACK_WEBHOOK_URL : any = ''
 
 if(environment == 'production'){
-  MY_SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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'
+  MY_SLACK_WEBHOOK_URL = ''
 }else{
  MY_SLACK_WEBHOOK_URL = 'test'
 }
